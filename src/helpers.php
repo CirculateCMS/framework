a43e80d549cfbe298383da75f3ee1d29fe63563d
Ashley Rich <hello@ashleyrich.com>
2018-04-06 15:59:01 +0100

    Stub out new helpers file

---
+++ src/helpers.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!function_exists('url')) {
+    /**
+     * Generate an absolute URL.
+     *
+     * @param string $path
+     * @return string
+     */
+    function url($path = '')
+    {
+        echo $path;
+    }
+}
